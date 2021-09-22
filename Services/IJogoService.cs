b4267acc83a0c0068ec0f10af043174af0bffc24
JulioBrizido <brizid@gmail.com>
2021-09-21 23:28:31 -0300

    Primeiro commit

---
+++ Services/IJogoService.cs
@@ -0,0 +1,18 @@
+﻿using ApiCatalogo.InputModel;
+using ApiCatalogo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiCatalogo.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
